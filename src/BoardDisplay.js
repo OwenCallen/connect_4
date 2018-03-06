bd5c0f6d08c8e9630955ebf80158956522a16828
NineteenGiraffes <NineteenGiraffes@gmail.com>
2018-03-06 14:55:38 -0500

    playable html board

--- src/BoardDisplay.js
+++ src/BoardDisplay.js
@@ -1,21 +1,32 @@
 
 
 let board = new CFBoard();
-displayBoard()
+displayBoard();
+
+function colClick(col){
+  if(board.canAddToCol(col)){
+    board.addToCol(col);
+    board.logBoard();
+    displayBoard();
+  } else {
+
+  }
+}
+
 
 function displayBoard(){
   document.getElementById('htmlBoard').innerHTML = generateHTML(board.arr)
-
 }
 
 function generateHTML(matrix) {											// creates html to display inputed matrix, output string
   var code = "";
   for (var row = 0; row < 6; row++) {
     for (var col = 0; col < 7; col++) {
-      code += "<div class='player" + matrix[col][row] + "' style='position:absolute; top:" + row*50 + "px; left:" + col*50 + "px' >";
-        matrix[col][row]
+      code += "<div class='players player" + matrix[col][row] + "' style='position:absolute; top:" +
+        (250 - (row*50)) + "px; left:" + col*50 + "px' >";
+      matrix[col][row]
       code += "</div>";
     }
   }
-  return code
+  return code;
 }

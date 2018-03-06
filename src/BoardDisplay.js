dc2227e483f7e496a9714d8935fd6a156fc1ed2e
NineteenGiraffes <NineteenGiraffes@gmail.com>
2018-03-06 17:06:48 -0500

    Html tweeks
    turn status with invalid move notification

--- src/BoardDisplay.js
+++ src/BoardDisplay.js
@@ -8,23 +8,32 @@ function colClick(col){
     board.addToCol(col);
     board.logBoard();
     displayBoard();
+    showTurn();
   } else {
-
+    document.getElementById('status').innerHTML = "<div class='player0'>Invalid move!</div>";
+    setTimeout(showTurn, 800);
   }
 }
-
 
 function displayBoard(){
   document.getElementById('htmlBoard').innerHTML = generateHTML(board.arr)
 }
 
-function generateHTML(matrix) {											// creates html to display inputed matrix, output string
+function showTurn(){
+  document.getElementById('status').innerHTML = "<div class='player" + board.turn + "'>Turn " + board.turn + "</div>";
+}
+
+
+function generateHTML(boardArr) {											// creates html to display inputed boardArr, output string
   var code = "";
-  for (var row = 0; row < 6; row++) {
-    for (var col = 0; col < 7; col++) {
-      code += "<div class='players player" + matrix[col][row] + "' style='position:absolute; top:" +
-        (250 - (row*50)) + "px; left:" + col*50 + "px' >";
-      matrix[col][row]
+  for (var col = 0; col < 7; col++) {
+    code += "<div style='position:absolute; top:0px; left:" + col*50 + "px' >";
+    code += "<button type='button' name='col" + col + "' onclick='colClick(" + col + ")'>&#x21E9</button>";
+    code += "</div>";
+    for (var row = 0; row < 6; row++) {
+      code += "<div class='piece player" + boardArr[col][row] + "' style='top:" +
+        (280 - (row*50)) + "px; left:" + col*50 + "px' >";
+      code += boardArr[col][row]
       code += "</div>";
     }
   }

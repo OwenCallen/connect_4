43ef85fbfa3b01112bae59bbe739847761e6098a
Owen <owencallen@gmail.com>
2018-03-06 12:10:44 -0500

    completed CFBoard constructor
    methods include:
    canAddToCol(num)
    addToCol(num)
    get arr()
    get turn()

--- src/CFBoard.js
+++ src/CFBoard.js
@@ -1,8 +1,41 @@
-function CFBoard (){
-  this.arr = new Array(7);
-  for(let i = 0 ; i<arr.length ; i++){
-    arr = [0, 0, 0, 0, 0, 0];
+class CFBoard {
+  constructor(){
+    this._arr = new Array(7);
+    for(let i = 0 ; i<_arr.length ; i++){
+      arr = [0, 0, 0, 0, 0, 0];
+    }
+    this._turn = 1;
+    this.compIsPlayer = 0
+  }
+  canAddToCol(num){
+    if(this._arr[num][5]==0){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  addToCol(num){
+    let player = this._turn;
+    for(let i = 0 ; i<this._arr.length ; i++){
+      if(this._arr[num][i]==0){
+        this._arr[num][i] = player;
+        i=this._arr.length;
+      }
+    }
+    if(player==1){
+      this._turn = 2;
+    }
+    else{
+      this._turn = 1;
+    }
+  }
+  get arr(){
+    return this._arr;
+  }
+  get turn(){
+    return this._turn;
   }
 }
-let board = new CFBoard();
-console.log('here is the board'+board);
+let myBoard = new CFBoard();
+console.log('here is the board'+myBoard.arr());

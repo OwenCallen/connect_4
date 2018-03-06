e5a19a95408af7a4461a71012a6600af934260be
NineteenGiraffes <NineteenGiraffes@gmail.com>
2018-03-06 11:18:15 -0500

    Revert "Merge branch 'master' of https://github.com/7buck7/connect_4"
    This reverts commit 9b446a2a22c74006e7c0d4deecc83a45289351cc, reversing
    changes made to a9521e29c36c314e7a84dd433c2c0cc96adf067f.

--- src/CFBoard.js
+++
@@ -1,9 +0,0 @@
-function CFBoard (){
-  this.arr = new Array(7);
-  for(let i = 0 ; i<arr.length ; i++){
-    arr = [0, 0, 0, 0, 0, 0];
-  }
-  )
-}
-let board = new CFBoard();
-console.log(board);
---
+++ src/scripts.js
@@ -0,0 +1,8 @@
+function CFBoard (){
+  this.arr=new Array(7);
+    this.arr.forEach(function(row){
+      row=new Array(6);
+    }
+  )
+}
+console.log('hello console');

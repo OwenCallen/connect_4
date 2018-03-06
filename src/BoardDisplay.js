4080f9a913643f08650c09b1811cbe530ee37701
NineteenGiraffes <NineteenGiraffes@gmail.com>
2018-03-06 12:23:36 -0500

    basic board display

---
+++ src/BoardDisplay.js
@@ -0,0 +1,21 @@
+
+
+let board = new CFBoard();
+displayBoard()
+
+function displayBoard(){
+  document.getElementById('board').innerHTML = generateHTML(board.arr())
+
+}
+
+function generateHTML(matrix) {											// creates html to display inputed matrix, output string
+  var code = "";
+  for (var row = 0; row < 6; row++) {
+    for (var col = 0; col < 7; col++) {
+      code += "<div class='player" + matrix[col][row] + "' style='position:absolute; top:" + row*50 + "px; left:" + col*50 + "px' >";
+        matrix[col][row]
+      code += "</div>";
+    }
+  }
+  return code
+}

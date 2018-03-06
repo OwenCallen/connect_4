7545e6fabc6ffcecd5fb1027d44c9d650406697f
Owen <owencallen@gmail.com>
2018-03-06 10:44:42 -0500

    testing console.log

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,8 @@
 function CFBoard (){
   this.arr=new Array(7);
-  this.arr.forEach(function
+    this.arr.forEach(function(row){
+      row=new Array(6);
+    }
   )
 }
+console.log('hello console');

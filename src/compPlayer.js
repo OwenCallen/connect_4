0251d49145eb12803cb25df245c3683a319742d5
Owen <owencallen@gmail.com>
2018-03-08 11:27:49 -0500

    built constructor and nextMove method

---
+++ src/compPlayer.js
@@ -0,0 +1,26 @@
+class randoComp {
+  constructor(playerNum){
+    if(playerNum==1 || playerNum==2){
+      this._playerNum = playerNum;
+    }
+  }
+
+  get playerNum(){
+    return this._playerNum;
+  }
+
+  nextMove(cFBoard){
+    if(cFBoard.turn()!==playerNum){
+      return "Not my turn.";
+    }
+    let moves = [];
+    for(let i = 0 ; i<7 ; i++){
+      if(cFBoard.canAddToCol(i)){
+        moves.push(i);
+      };
+    }
+    move = Math.floor(Math.random()*moves.length);
+    return move;
+  }
+
+}
